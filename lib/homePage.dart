b0d7cdace62775aef6fba237ea8efffaee915564
MONSTER <bahricanates95@gmail.com>
2025-04-10 01:45:53 +0300

    izinler

---
+++ lib/homePage.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+  GoogleMapController? _mapController;
+  LatLng _initialPosition = const LatLng(41.015137, 28.979530); // İstanbul
+
+  @override
+  void initState() {
+    super.initState();
+    _requestLocationPermission();
+  }
+
+  void _requestLocationPermission() async {
+    var status = await Permission.location.request();
+    if (status.isGranted) {
+      print("Konum izni verildi.");
+    } else {
+      print("Konum izni reddedildi.");
+    }
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.menu),
+          onPressed: () {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text('Menü tıklandı!')),
+            );
+          },
+        ),
+        title: const Text('Here'),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.message),
+            onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(content: Text('Mesajlar açıldı!')),
+              );
+            },
+          ),
+        ],
+      ),
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: _initialPosition,
+          zoom: 12,
+        ),
+        myLocationEnabled: true,
+        onMapCreated: (GoogleMapController controller) {
+          _mapController = controller;
+        },
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Ana Sayfa',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.store),
+            label: 'Mağaza',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.camera, size: 40),
+            label: 'Fotoğraf',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.heart_broken_outlined),
+            label: 'Beğeniler',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profil',
+          ),
+        ],
+      ),
+    );
+  }
+}
